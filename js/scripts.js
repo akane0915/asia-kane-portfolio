40a1fa93e0dc487e3631acdfd3f38da91d1f7476
akane0915 <asialkane@gmail.com>
2017-04-20 19:11:21 -0700

    refactor javascript code for portfolio section to hide images on mousehover and display the portfolio project description

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 //UI Logic
 $(document).ready(function(){
   $(".portfolio-image").mouseover(function() {
-    $(this).children(".description-on-hover").show();
-    $(this).addClass("dim-on-hover");
+    $(this).children(".description-on-hover").fadeIn();
+    $(this).addClass("dim-on-hover").fadeIn();
   }).mouseout(function() {
-    $(this).children(".description-on-hover").hide();
-    $(this).removeClass("dim-on-hover");
-});
-
+    $(this).children(".description-on-hover").fadeOut();
+    $(this).removeClass("dim-on-hover").delay(5000);
+  });
 }); //Doc ready close
