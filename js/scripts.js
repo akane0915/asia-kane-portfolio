aefb3c2dea5f7b5c05891c1727f2649c45c2d299
akane0915 <asialkane@gmail.com>
2017-04-22 16:28:12 -0700

    fix the mouseover flashing image in portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //UI Logic
 $(document).ready(function(){
   $(".portfolio-image").mouseover(function() {
-    $(this).children(".description-on-hover").fadeIn();
-    $(this).addClass("dim-on-hover").fadeIn();
+    $(this).children(".description-on-hover").show();
+    $(this).addClass("dim-on-hover");
   }).mouseout(function() {
-    $(this).children(".description-on-hover").fadeOut();
-    $(this).removeClass("dim-on-hover").delay(5000);
+    $(this).children(".description-on-hover").hide();
+    $(this).removeClass("dim-on-hover");
   });
 }); //Doc ready close
