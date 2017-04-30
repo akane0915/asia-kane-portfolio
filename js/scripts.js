4d9ba6de7fe16417a8f8eea915282354c9b03980
akane0915 <asialkane@gmail.com>
2017-04-29 21:45:59 -0700

    make nav bar collapse to drop down on smaller screens and add scroll function in jquery when user clicks on navigation link

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 //UI Logic
 $(document).ready(function(){
+  // Navigation bar scroll to section on click
+  $("#scroll-to-about-section").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#about-section").offset().top
+      }, 1000);
+  });
+  $("#scroll-to-work-section").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#work-section").offset().top
+      }, 1000);
+  });
+  $("#scroll-to-portfolio-section").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#portfolio-section").offset().top
+      }, 1000);
+  });
+  $("#scroll-to-contact-section").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#contact-section").offset().top
+      }, 1000);
+  });
+
+  // Portfolio section display description on hover
   $(".portfolio-image").mouseover(function() {
     $(this).children(".description-on-hover").show();
     $(this).addClass("dim-on-hover");
